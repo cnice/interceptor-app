573c4286e2b91507fa22a436ee3b978f09d043a4
cnice <castud@gmail.com>
2018-07-29 23:12:47 -0400

    logic for log service

--- src/app/http-interceptors/log-interceptor.ts
+++ src/app/http-interceptors/log-interceptor.ts
@@ -1,9 +1,10 @@
 import { HttpInterceptor, HttpHandler, HttpRequest, HttpEvent, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
+import { LoggerService } from '../services/logger.service';
 
 export class LogInterceptor implements HttpInterceptor {
-    constructor() {}
+    constructor(private logService: LoggerService) {}
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         const started = Date.now();
@@ -11,7 +12,8 @@ export class LogInterceptor implements HttpInterceptor {
             tap(event => {
                 if (event instanceof HttpResponse) {
                     const elapsed = Date.now() - started;
-                    console.log(`Request for ${req.urlWithParams} took ${elapsed} ms.`);
+                    this.logService.add(`Started - ${elapsed}`);
+                    this.logService.add(`Request for ${req.urlWithParams} took ${elapsed} ms.`);
                 }
             })
         );
